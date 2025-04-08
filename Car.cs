dc4b2456b360e4aa838f8616cfb304afc123d574
Cas0n-McVey <absolgamer25@gmail.com>
2025-04-08 13:06:25 -0500

    Properties

---
+++ Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_McVey_Cason
+{
+    class Car
+    {
+        // Privated
+        private string model;
+
+        // any string with Model in it get updated and returned
+        public string Model { get { return model; } set { model = value; }  }
+
+        // This code is an automatic property
+        public string Make { get; set; }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Properties_McVey_Cason
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This code is creating a new object for car
+            Car car = new Car();
+            car.Make = "Ford";
+            car.Model = "Mustang";
+
+            // Prints out car2 new code
+            Console.WriteLine($"Make: {car.Make}, Model: {car.Model}");
+
+            // This code is creating a new object for car2
+            Car car2 = new Car();
+            car2.Make = "Chevy";
+            car2.Model = "Camero";
+
+            // Prints out car2 new code
+            Console.WriteLine($"Make: {car2.Make}, Model: {car2.Model}");
+
+            // This code is creating a new object for car3
+            Car car3 = new Car();
+            Console.WriteLine($"Make: {car3.Make}, Model: {car3.Model}");
+        }
+    }
+}
